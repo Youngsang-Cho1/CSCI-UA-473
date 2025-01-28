ee22a10606b98d3253bd8184316b3f8fb599b16a
Youngsang-Cho1 <youngsang1211@gmail.com>
2025-01-28 17:48:08 -0500

    Updated boardFromString, rowColToIndex, indexToRowCol, setBoardCell functions

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -8,6 +8,67 @@ function generateBoard(rows, cols, initialValue) {
   return repeat(blankValue, rows * cols);
 }
 
+function boardFromString(s) {
+  const validChar = "XO ";
+  const boardArray = s.split("");
+  for (i of boardArray) {
+    if (!validChar.includes(i)) {
+      return null;
+    }
+  }
+  const size = Math.sqrt(s.length);
+  if (!Number.isInteger(size)) {
+    return null;
+  }
+  return boardArray;
+  
+}
+
+function rowColToIndex(board, row, col) {
+  const size = Math.sqrt(board.length);
+  return row * size + col;
+
+}
+
+function indexToRowCol(board, i) {
+  const size = Math.sqrt(board.length);
+  const row = Math.floor(i / size);
+  const col = i % size;
+  return {"row" : row, "col" : col}
+}
+
+function setBoardCell(board, letter, row, col) {
+  const copy = board[0, -1];
+  const size = Math.sqrt(board.length);
+  board[row * size + col] = letter;
+  return board;
+}
+
+function algebraicToRowCol(algebraicNotation) {
+  
+
+}
+
+function placeLetter(board, letter, algebraicNotation) {
+
+}
+
+function getWinner(board) {
+
+}
+
+function isBoardFull(board) {
+
+}
+
+function isValidMove(board, algebraicNotation) {
+
+}
+
+
+
+
 export {
   generateBoard
 }
+
