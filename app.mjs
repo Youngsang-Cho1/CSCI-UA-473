54ae72aa088d9e655331be87957a02804fa96676
Youngsang-Cho1 <youngsang1211@gmail.com>
2025-02-23 19:50:18 -0500

    added package.json, .gitignore, etc. completed part 2 and 3. created the layout hbs and quiz hbs.

--- app.mjs
+++ app.mjs
@@ -1,11 +1,34 @@
 // app.mjs
-
+import express from 'express'
+import hbs from 'hbs'
 export let server = null;
 export const app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 // Implement the decorate function
 export const decorate = (answer, correct) => {
     return ""
 }
 
+app.use((req, res, next) => {
+    console.log(`Method: ${req.method}`)
+    console.log(`Path: ${req.path}`)
+    console.log(`Query: ${req.query}`)
+    next()
+})
+
+
+app.get('/quiz', (req, res) => {
+    res.render('quiz')
+})
+
+
+
+server = app.listen(3000, () => {
+    console.log("Server started; type CTRL+C to shut down");
+});
 // Continue with the rest of the code
+// cd desktop/homework04-Youngsang-Cho1
--- query.mjs
+++ query.mjs
@@ -1,1 +1,12 @@
 // query.mjs
+import { v4 as uuidv4 } from 'uuid';
+
+export class Query {
+    constructor (id, question, genre, answers){
+        this.id = id;
+        this.question = question;
+        this.genre = genre;
+        this.answers = answers;
+    }
+
+}
