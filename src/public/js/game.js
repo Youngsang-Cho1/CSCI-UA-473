409830c29c39086aab0ab3e65d55bf5e0f866a0e
Youngsang-Cho1 <youngsang1211@gmail.com>
2025-04-05 18:22:30 -0400

    working on ingame logic. committing to save my progress

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -1,10 +1,12 @@
 // game.js
 const startButton = document.querySelector(".play-btn")
 
+
 startButton.addEventListener('click', function() {
     const preset = document.querySelector("#card-faces").value
     const cardNum = Number(document.querySelector("#total-cards").value);
     const turnNum = Number(document.querySelector("#max-turns").value);
+    let counter = cardNum/2;
 
     if (cardNum % 2 != 0 || cardNum < 2 || cardNum > 36) {
         document.querySelector(".error-message").classList.remove("hidden");
@@ -34,6 +36,46 @@ startButton.addEventListener('click', function() {
             document.querySelector(".error-message").classList.remove("hidden");
             return;
         }
+        generateBoard(cardNum, presetList, counter)
     }
-    // gamefunction....
+    //else... random symbols
+    
 });
+
+function generateBoard(cardNum, symbols, counter) {
+    const gameBoard = document.querySelector(".game");
+    const result = document.querySelector(".result");
+
+    gameBoard.style.display = "grid";
+
+    let flippedCard = [];
+
+    for (let i = 0; i < cardNum; i++) {
+        const randomIdx = Math.floor(Math.random() * symbols.length);
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerText = symbols[randomIdx];
+        symbols.splice(randomIdx, 1);
+
+        card.addEventListener("click", function() {
+            if (!card.classList.contains("flipped") && flippedCard.length < 2) {
+                card.classList.toggle("flipped");
+                flippedCard.push(card)
+            }
+            if (flippedCard.length === 2) {
+                if (flippedCard[0].innerText === flippedCard[1].innerText) {
+                    result.innerText = "Matches. Press OK";
+                }
+                else {
+                    result.innerText = "Does not match. Press OK";
+                }
+                flippedCard = []
+                counter++;
+            }
+
+        })
+        gameBoard.appendChild(card);
+    }
+
+}
+// cd desktop/homework06-Youngsang-Cho1
